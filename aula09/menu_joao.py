94714c5970cd0f8a7ae594e8a4b542d11cee72a5
JOÃO <jangelomoreira@gmail.com>
2025-11-18 19:52:26 -0300

    Joao:Desafio aula 09 - Menu SCP

---
+++ aula09/menu_joao.py
@@ -0,0 +1,13 @@
+# Arquivo: menu_joao.py
+# Autor: João Emanuel
+# Data: 17/11/2025
+print("*" * 40)
+print("*SC - Sistema de COntrole de Pátio*")
+print(f"Por: João Emanuel                *")
+print("*" * 40)
+print(" 1 - Cadastrar Bloco")
+print("2 - Listar Blocos")
+print("3 - Buscar Bloco")
+print("S - Sair")
+print("*" * 40)
+input ("Pressione Enter para continuar...")

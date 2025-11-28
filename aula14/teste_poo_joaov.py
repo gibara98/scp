5a56dee32c3e00c74d89858bc731c7f4c4a3602c
Joao V <vitortullijoao@gmail.com>
2025-11-27 21:52:17 -0300

    Joao: Aula 14 poo

---
+++ aula14/teste_poo_joaov.py
@@ -0,0 +1,7 @@
+class Bloco:
+     def __init__(self, codigo, preco_m2, largura, altura):
+         self.codigo = codigo
+         self.preco_m2 = preco_m2
+         self.largura = largura
+         self.altura = altura
+       
